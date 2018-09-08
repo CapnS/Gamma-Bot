a1569a45b634d8a74ba351e825641214524cb4ea
Xua Yraili <williamblakelock.wb@gmail.com>
2018-09-09 09:48:42 +1200

    rename command to fit negative numbers

--- cogs/eco.py
+++ cogs/eco.py
@@ -104,7 +104,7 @@ class Economy:
 
     @commands.command(hidden=True)
     @commands.is_owner()
-    async def baladd(self, ctx, amount: int, user: discord.Member=None):
+    async def baladjust(self, ctx, amount: int, user: discord.Member=None):
         await self.bot.db.execute("UPDATE economy SET balance=balance+$1 WHERE userid=$2;", amount, user.id)
         await ctx.send("\U0001f44c")
 
